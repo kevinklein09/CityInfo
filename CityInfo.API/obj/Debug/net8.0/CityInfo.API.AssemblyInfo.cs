de13a9c569e49512ce0d68676831bf263daa785f
Kevin Klein <kklein166@gmail.com>
2024-08-23 14:59:37 -0500

    getting a file

--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc326005b5b1f9d566ef53e79400f368a6cb1ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163a68413ef3cc490fd35847aabed926bd3c807e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
