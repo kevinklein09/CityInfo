79f028c4b79b9fb667f9266681a34e315bd40f88
Kevin Klein <kklein166@gmail.com>
2024-08-23 15:02:08 -0500

    merged

--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163a68413ef3cc490fd35847aabed926bd3c807e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4bb47902b2c3f95d0e2cd426285d1cdac79453")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
