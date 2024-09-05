996d2bf7bc106373e43adc1f42bdc10a1c2c1c0c
Kevin Klein <kevink@e-claim.com>
2024-09-05 12:48:25 -0500

    deleting a resource

--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e480ab7196590c13689b02b26fbf216fddba22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a83818141e1d8c5980c980d1dd1a1ea42833c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
